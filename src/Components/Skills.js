465f5794bbd58873a5b44e88a3f63999469e3b3b
Adam Gorski <adamgorski1256@gmail.com>
2021-01-06 18:46:19 +0100

    add responsiveness for skills section

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -20,39 +20,42 @@ const Skills = () => {
         <motion.div
             exit="out" animate="in" initial="initial" variants={animationTwo} transition={transition}
             className="skills">
+
             <h1> My skills</h1>
-            <div className="skills__images_container">
-                <h2>Front end</h2>
-                <div className="skills__images">
-                    <ul>
-                        <li><img src={html} alt="html" /> </li>
-                        <li><img src={css} alt="css" /></li>
-                        <li><img src={sass} alt="sass" /></li>
-                        <li><img src={js} alt="java script" /></li>
-                        <li><img src={react} alt="react" /></li>
-                    </ul>
+            <div className="skills__container">
+                <div className="skills__images_container">
+                    <h2>Front end</h2>
+                    <div className="skills__images">
+                        <ul>
+                            <li><img src={html} alt="html" /> </li>
+                            <li><img src={css} alt="css" /></li>
+                            <li><img src={sass} alt="sass" /></li>
+                            <li><img src={js} alt="java script" /></li>
+                            <li><img src={react} alt="react" /></li>
+                        </ul>
+                    </div>
+                    <h2>General</h2>
+                    <div className="skills__images">
+                        <ul>
+                            <li><img src={webpack} alt="webpack" /></li>
+                            <li><img src={git} alt="git" /></li>
+                        </ul>
+                    </div>
                 </div>
-                <h2>General</h2>
-                <div className="skills__images">
-                    <ul>
-                        <li><img src={webpack} alt="webpack" /></li>
-                        <li><img src={git} alt="git" /></li>
-                    </ul>
+                <div className="skills__text_container">
+                    <p>&emsp;&emsp;&emsp;In my work, I use HTML, CSS, (SCSS) which are absolutely essential.
+                        I also have a strong foundation in JavaScript and React, in which I can write not
+                        too complicated applications. All my projects are responsive in order to be user-friendly.
+                    </p>
+
+                    <p>&emsp;&emsp;&emsp;I am very motivated and enjoy to solve problems. If I don’t know how to do something I am looking for a solution on
+                    Stack Overflow, Google or docs. I have good communication skills.
+                    I am aware of the fact that soft skills are very important in everyday developer’s life.
+                        I have basic understanding of Scrum and Agile methodologies.
+                    </p>
+
+                    <p>I'm constantly learning and improving my skills. It's FUN!</p>
                 </div>
-            </div>
-            <div className="skills__text_container">
-                <p>&emsp;&emsp;&emsp;In my work, I use HTML, CSS, (SCSS) which are absolutely essential.
-                     I also have a strong foundation in JavaScript and React, in which I can write not
-                      too complicated applications. All my projects are responsive in order to be user-friendly.
-                </p>
-
-                <p>&emsp;&emsp;&emsp;I am very motivated and enjoy to solve problems. If I don’t know how to do something I am looking for a solution on
-                Stack Overflow, Google or docs. I have good communication skills.
-                I am aware of the fact that soft skills are very important in everyday developer’s life.
-                    I have basic understanding of Scrum and Agile methodologies.
-                </p>
-
-                <p>I'm constantly learning and improving my skills. It's FUN!</p>
             </div>
         </motion.div>
     );
