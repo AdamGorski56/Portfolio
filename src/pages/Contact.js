9c10ff814bc1083e784307dd69c4b8a99ff1f642
Adam Gorski <adamgorski1256@gmail.com>
2021-01-14 15:28:58 +0100

    fix responsiveness and styles for mywork and contact section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -27,8 +27,8 @@ const Contact = () => {
                     <h2>Feel free to contact me</h2>
                     <div className="contact__info_content_container">
 
-                        <span className="contact__email"><i class="fa fa-envelope-open"></i>adamgorski1256@gmail.com</span>
-                        <span className="contact__phone"><i class="fa fa-phone"></i>+44 78 2710 88 35</span>
+                        <span className="contact__email"><i class="fa fa-envelope-open"></i><div className="line"></div>adamgorski1256@gmail.com</span>
+                        <span className="contact__phone"><i class="fa fa-phone"></i><div className="line"></div>+44 78 2710 88 35</span>
                         <span className="contact__social_media"> <a href="https://www.linkedin.com/in/adam-gorski-a58b05202/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a><a href="https://github.com/AdamGorski56/" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></span>
 
                         <a href="mailto:adamgorski1256@gmail.com"><motion.button whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px 0px rgb(3, 187, 3)", }} className="contact__email_btn">Send Email</motion.button></a>
